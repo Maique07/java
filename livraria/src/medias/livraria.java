37bb215d831c657906f163b10d949f83da724eae
Maique07 <152615451+Maique07@users.noreply.github.com>
2023-12-06 18:21:14 -0300

    Add files via upload

---
+++ livraria/src/medias/livraria.java
@@ -0,0 +1,27 @@
+package medias;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Maique
+ */
+public class livraria {
+
+    public static void main(String[] args) {
+        int qt;
+        String a;
+        a = JOptionPane.showInputDialog("Informe a quantidade de livro");
+        qt = Integer.parseInt(a);
+        float v1 = (float) ((qt * 0.25) + 7.50);
+        float v2 = (float) ((qt * 0.50) + 2.50);
+        if (v1 < v2) {
+            JOptionPane.showMessageDialog(null, "o melhor desconto é a 1 opção");
+        }
+        if (v2 < v1) {
+            JOptionPane.showMessageDialog(null, "o melhor desconto é a 2 opção");
+        }
+
+    }
+
+}
